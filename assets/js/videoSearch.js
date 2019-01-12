2270766788ddba54d3485d30443dfc866456e8c8
mhoef <mhoefer1@gmail.com>
2019-01-12 13:07:39 -0500

    youtube api and geocoding

--- assets/js/videoSearch.js
+++ assets/js/videoSearch.js
@@ -2,13 +2,24 @@ $(document).ready(function () {
   $(document).on("click", "#search", search);
 });
 
-function buildVideoSearchUrl(lat, long, keywords, radius){
-  
-  var apiKey = "AIzaSyBY4WNlt7XxKT8nkIktT9cJC7DEgDr8EZ0";
-  var queryURL = "https://www.googleapis.com/youtube/v3/search/?part=snippet&type=video&key=" + apiKey + 
-  "&location=" + lat + "%2C" + long + 
-  "&locationRadius=" + radius + 
-  "&q=" + keywords;
+function buildVideoSearchUrl(lat, long, keywords, radius) {
+
+  var apiKey = "AIzaSyCdwC23T8LG-ANlisnBqfODfqoauzfNJO8";
+  var queryURL = "https://www.googleapis.com/youtube/v3/search/?part=snippet&type=video&key=" + apiKey +
+    "&location=" + lat + "%2C" + long +
+    "&locationRadius=" + radius +
+    "&q=" + keywords;
+
+  return queryURL;
+
+}
+
+function buildVideoLocationSearchUrl(id) {
+
+  var apiKey = "AIzaSyCdwC23T8LG-ANlisnBqfODfqoauzfNJO8";
+  var queryURL = "https://www.googleapis.com/youtube/v3/videos?part=recordingDetails" +
+    "&id=" + id +
+    "&key=" + apiKey;
 
   return queryURL;
 
@@ -22,26 +33,49 @@ function search() {
 
   // This line will grab the text from the input box
   var keywords = $("#keywords").val().trim();
-  var lat = $("#lat").val().trim();
-  var long = $("#long").val().trim();
+  var address = $("#address").val().trim();
   var radius = $("#radius").val().trim();
 
-  var queryURL = buildVideoSearchUrl(lat, long, keywords, radius);
-  console.log(queryURL);
-  // Performing our AJAX GET request
+  var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyDgDiGe1sPYYqpbGQCbUR5W9gUVrv7L1Qk';
   $.ajax({
-    url: queryURL,
+    url: geocodeUrl,
     method: "GET"
   })
-    // After the data comes back from the API
+
     .then(function (response) {
-      // Storing an array of results in the results variable
-      var results = response.items;
-      console.log(results);
-      // Looping over every result item
-      for (var i = 0; i < results.length; i++) {
-        var id = results[i].id.videoId;
-        var myLocation = videoLocationSearch(id);
+      if (response.status == 'OK') {
+
+        console.log(response);
+        var lat = response.results[0].geometry.location.lat.toString();
+        var long = response.results[0].geometry.location.lng.toString();
+
+        $.ajax({
+          url: buildVideoSearchUrl(lat, long, keywords, radius),
+          method: "GET"
+        })
+          // After the data comes back from the API
+          .then(function (response) {
+            // Storing an array of results in the results variable
+            var results = response.items;
+            console.log(results);
+
+            for (var i = 0; i < results.length; i++) {
+              var id = results[i].id.videoId;
+              
+              //Get video location details
+              $.ajax({
+                url: buildVideoLocationSearchUrl(id),
+                method: "GET"
+              })
+                .then(function (myResponse) {
+                  var location = myResponse.items[0].recordingDetails.location;
+                  console.log(location);
+
+                });
+            }
+          });
       }
     });
+
+
 }
