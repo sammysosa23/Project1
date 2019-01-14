3e6598d7aeb7e982c357c195333e0722626ff36a
Sam <sam.terrazas@yahoo.com>
2019-01-14 10:05:19 -0500

    updated HTML to include lightbox/sign-up, created a database to save users input, created a emptyGoogleDiv to populate the html for map

--- appProject.js
+++ appProject.js
@@ -1,15 +1,52 @@
-// LIGHTBOX 
 $(document).ready(function() {
 
-        var lightBox = $(".lightbox");
+    // INITIALIZE FIREBASE
+    var config = {
+        apiKey: "AIzaSyCtHL7f2YUS9l46Rdo3ilu9v8VJLbMOMPo",
+        authDomain: "projectonelightbox.firebaseapp.com",
+        databaseURL: "https://projectonelightbox.firebaseio.com",
+        projectId: "projectonelightbox",
+        storageBucket: "",
+        messagingSenderId: "369764684225"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
 
     // LIGHTBOX FUNCTION //
     $(function() {
         $(".lightbox").delay(500).fadeIn(1000);
 
-        $(".enterButton").on("click", function() {
-          lightBox.animate({ height: "300px"});
-            alert("test");
+        // LIGHTBOX - SUBMIT BUTTON ON CLICK EVENT 
+        $(".submitButton").on("click", function(event) {
+            event.preventDefault();
+            // SUBMIT BUTTON IS CLICKED LIGHTBOX WILL HIDE 
+            $(".lightbox").hide();
+            // alert("you clicked me");
+
+            // GRABBING USERS INPUT IN FORM
+            var submitName = $(".name").val().trim();
+            var submitEmail = $(".email").val().trim();
+
+            // PUSH INFO OF USER TO DATABASE
+            database.ref().push( {
+                submitName: submitName,
+                submitEmail: submitEmail
+            });
         });
     });
 });
+
+// EMPTY DOM TO BE POPULATED BY THE GOOGLE API
+// googleTargetDiv.textContent = "TEST";    TESTING TO MAKE SURE IT WORKED CORRECTLY AND IN THE CORRECT SPOT / NEED TO DELETE THIS
+$("#emptyGoogleDiv").html(" ");
+
+
+
+
+
+
+
+
+
+
