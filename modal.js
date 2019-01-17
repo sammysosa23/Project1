4a6a892c033fa023c538de76570f9400ad86733f
skendall <shannonckendall@gmail.com>
2019-01-17 17:49:35 -0500

    Changes

--- modal.js
+++ modal.js
@@ -1,70 +1,64 @@
 //For Modal - JS JQuery
 
-var map;
-var infowindow;
+    var map;
+    var service;
+    var infowindow;
 //Original Google Map Loaction
-function initMap() {
-  var pyrmont = {
-    lat: -33.867,
-    lng: 151.195
-  };
-  console.log(pyrmont);
+    function initMap() {
+      var atlanta = {
+        lat: 33.753746,
+        lng: -84.386330,
+      };
+//Original Google Map Center and Distance
+      map = new google.maps.Map(document.getElementById('mapCanvas'), {
+        center: atlanta,
+        zoom: 15
+      });
 
-  //Original Google Map Center and Distance
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: pyrmont,
-    zoom: 15
-  });
-  console.log('map');
-  //Google Maps Services
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: pyrmont,
-    radius: 500,
-    type: ['restaurants', 'bars']
-  }, callback);
-  console.log('restaurants', 'bars');
-}
-//Google Maps Markers
-function callback(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+      infowindow = new google.maps.InfoWindow();
+//Google Maps Services
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: atlanta,
+        radius: 500,
+        type: ['restaurant'],
+      }, callback);
+
+      service.nearbySearch({
+        location: atlanta,
+        radius: 500,
+        type: ['bar'],
+      }, callback);
+
     }
-  }
-  console.log(google.maps.places.PlacesServiceStatus.OK);
-}
 
-//Google Maps Marker Creation
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-  console.log(marker);
-  //Google Maps Marker Creation
-  google.maps.event.addListener(marker, 'click', function () {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-  console.log(marker);
-}
+    function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    }
+//Google Maps Markers
+    function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+      });
+      console.log(marker)
 
-//Modal Call Up 
-$(document).ready(function () {
-  // Re-init map before show modal
-  $('#myModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    $("#location-map").css("width", "100%");
-    $("#map_canvas").css("width", "100%");
-  });
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+          'Place Rating: ' + place.rating + '<br>' +
+          place.formatted_address, +'</div>');
+        infowindow.open(map, this);
+      });
+    }
 
-
-  // Trigger map resize event after modal shown
-  $('#myModal').on('shown.bs.modal', function () {
+//Modal
+$(".modal-content").on('click',"shown.bs.modal", function () {
     google.maps.event.trigger(map, "resize");
     map.setCenter(map);
   });
-});
+  console.log(".modal-content");
